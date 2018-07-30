5f2b4168d85af9780894e5024cd686aac7bab9c4
Aghasy <agasy18@gmail.com>
2018-07-31 00:26:27 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -232,14 +232,24 @@ def handle_command(command, event):
 
 
 if __name__ == "__main__":
-    if slack_client.rtm_connect(with_team_state=False):
-        print("Starter Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        starterbot_id = slack_client.api_call("auth.test")["user_id"]
-        while True:
-            command, event = parse_bot_commands(slack_client.rtm_read())
-            if command is not None:
-                handle_command(command, event)
-            time.sleep(RTM_READ_DELAY)
-    else:
-        print("Connection failed. Exception traceback printed above.")
+    while True:
+        try:
+            if slack_client.rtm_connect(with_team_state=False):
+                print("Starter Bot connected and running!")
+                # Read bot's user ID by calling Web API method `auth.test`
+                starterbot_id = slack_client.api_call("auth.test")["user_id"]
+                while True:
+                    command, event = parse_bot_commands(slack_client.rtm_read())
+                    if command is not None:
+                        handle_command(command, event)
+                    time.sleep(RTM_READ_DELAY)
+            else:
+                print("Connection failed. Exception traceback printed above.")
+        except KeyboardInterrupt:
+            raise
+        except SystemExit:
+            raise
+        except:
+            traceback.print_tb()
+            time.sleep(10)
+            pass
